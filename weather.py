161581f10601819d004dbe40b11572bba31837b9
Holyfundz <jerrychris214@gmail.com>
2025-07-08 10:27:05 +0100

    Update weather message and handle errors better

--- weather.py
+++ weather.py
@@ -1,14 +1,15 @@
 import requests
 from rich import print
+def weather(city):
+    city = input("Enter the name of city: ")
+    url = f"https://wttr.in/{city}?format=3"
 
-city = "Lagos"
-url = f"https://wttr.in/{city}?format=3"
-
-try:
-    response = requests.get(url)
-    if response.ok:
-        print(f"[bold cyan]Weather:[/bold cyan] {response.text}")
-    else:
-        print("[red]Failed to fetch weather info[/red]")
-except Exception as e:
-    print(f"[red]Error:[/red] {e}")
+    try:
+        response = requests.get(url)
+        if response.ok:
+            print(f"[bold cyan]Weather:[/bold cyan] {response.text}")
+        else:
+            print("[red]Failed to fetch weather info[/red]")
+    except Exception as e:
+        print(f"[red]Error:[/red] {e}")
+ 
