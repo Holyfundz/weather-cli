d885922e76f5d3f5ebeed0edaaec1009d7fb0123
Holyfundz <jerrychris214@gmail.com>
2025-07-08 10:03:05 +0100

    Initial commit of weather-cli

---
+++ weather.py
@@ -0,0 +1,14 @@
+import requests
+from rich import print
+
+city = "Lagos"
+url = f"https://wttr.in/{city}?format=3"
+
+try:
+    response = requests.get(url)
+    if response.ok:
+        print(f"[bold cyan]Weather:[/bold cyan] {response.text}")
+    else:
+        print("[red]Failed to fetch weather info[/red]")
+except Exception as e:
+    print(f"[red]Error:[/red] {e}")
